5f22d7e2d5549a97c10b90e9e4c42a1616aa1018
joshsawfulcode <joshtaylor361@gmail.com>
2020-10-29 11:29:09 -0700

    Main Menu Completed

--- app/src/main/java/ca/cmpt276/project/MainActivity.java
+++ app/src/main/java/ca/cmpt276/project/MainActivity.java
@@ -2,7 +2,13 @@ package ca.cmpt276.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import Model.CoinFlip;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        The following is an outline of how the buttons in activity_main could
+        be used. Feel free to edit and remove.
+
+
+        Button configureChildren = findViewById(R.id.configureChildren);
+        configureChildren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = ChildManager.makeIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+
+        Button coinFlip = findViewById(R.id.coinFlip);
+        coinFlip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = CoinFlip.makeIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+
+        Button timeoutTimer = findViewById(R.id.timeoutTimer);
+        timeoutTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = ChildManager.makeIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+
+         */
+
+
     }
 }
