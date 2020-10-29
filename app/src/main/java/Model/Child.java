f65bdc56668e7a5e17e929fc3eb325fecc86bfa4
joshsawfulcode <joshtaylor361@gmail.com>
2020-10-29 12:18:56 -0700

    ChildManager outline

---
+++ app/src/main/java/Model/Child.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Child {
+    String name;
+
+    private Child(String name){
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
