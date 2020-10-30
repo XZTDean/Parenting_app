4f20a3839eeae28fb9079558fab86e6ee08dc2fa
Neil <xda22@sfu.ca>
2020-10-29 23:49:11 -0700

    Update Class CoinFlip

--- app/src/main/java/ca/cmpt276/project/model/CoinFlip.java
+++ app/src/main/java/ca/cmpt276/project/model/CoinFlip.java
@@ -7,26 +7,44 @@ import java.util.List;
 import java.util.Random;
 
 public class CoinFlip {
+
+    private static CoinFlip instance;
+
+    public static CoinFlip getInstance() {
+        if (instance == null){
+            instance = new CoinFlip();
+        }
+        return instance;
+    }
+
+
     private List<CoinFlipStats> FlipList = new ArrayList<>();
+
+
 
     public CoinFlipStats flipCoin(String childName, int choice){
         String timedate = java.text.DateFormat.getDateTimeInstance().format(new Date());
         Random rand = new Random();
-        int result = rand.nextInt(2);
+        int result = rand.nextInt(1);
         return new CoinFlipStats(timedate,childName,choice,result);
 
     }
+
     public void addStats(CoinFlipStats c){
         FlipList.add(c);
     }
 
 
-
-    public String[] displayList(){
+    public String[] getItemInList(){
         String[] tempList = new String[FlipList.size()];
         for(int i = 0; i<FlipList.size();i++){
-            tempList[i]= FlipList.get(i).getChildName();
-            
+            tempList[i]= "at "+ FlipList.get(i).getFlipTime()+" "+ FlipList.get(i).getChildName()+
+                    " gets to pick "+FlipList.get(i).getChoice()+" and the result turns out to be "+FlipList.get(i).getResult();
+            if(FlipList.get(i).getChoice() == FlipList.get(i).getResult()){
+                //To do: Add check mark icon if choice == result.
+            }
+
+
 
         }
         return tempList;
