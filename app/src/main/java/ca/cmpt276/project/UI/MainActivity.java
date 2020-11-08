4eac2c46a4c24ef56b652ecac8779af65d412a9d
Neil <xda22@sfu.ca>
2020-11-07 17:22:43 -0800

    Create a new activity which displays the history of coinFlips.

--- app/src/main/java/ca/cmpt276/project/UI/MainActivity.java
+++ app/src/main/java/ca/cmpt276/project/UI/MainActivity.java
@@ -2,8 +2,11 @@ package ca.cmpt276.project.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -14,6 +17,7 @@ import java.util.ArrayList;
 import ca.cmpt276.project.R;
 import ca.cmpt276.project.model.Child;
 import ca.cmpt276.project.model.ChildManager;
+import ca.cmpt276.project.model.CoinFlip;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -47,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         coinFlip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = CoinFlip.makeIntent(MainActivity.this);
+                Intent intent = flipHistory.makeIntent(MainActivity.this);
                 startActivity(intent);
             }
         });
