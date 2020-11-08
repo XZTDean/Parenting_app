dc61fecd12252aed09e5a10c0b376982b10daa5b
zetix <zetix@sfu.ca>
2020-11-09 04:02:54 +0800

    Create ChildManagerActivity

---
+++ app/src/main/java/ca/cmpt276/project/UI/ChildManagerActivity.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.project.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.project.R;
+
+public class ChildManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_manager);
+    }
+}
