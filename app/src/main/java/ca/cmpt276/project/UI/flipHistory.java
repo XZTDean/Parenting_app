fc79a719c6bbb3a2f831169db505b7b571515484
Neil <xda22@sfu.ca>
2020-11-09 05:12:15 -0800

    Update flipHistory to have an imageView

--- app/src/main/java/ca/cmpt276/project/UI/flipHistory.java
+++ app/src/main/java/ca/cmpt276/project/UI/flipHistory.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +27,10 @@ public class flipHistory extends AppCompatActivity {
     private CoinFlip coinFlipManager = CoinFlip.getInstance();
     List<CoinFlipStats> myList = coinFlipManager.getList();
 
+    //Add a case to Test if populateListView works. Will delete later.
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +39,10 @@ public class flipHistory extends AppCompatActivity {
         //Add a case to Test if populateListView works. Will delete later.
         CoinFlipStats flip1 = new CoinFlipStats("timedate","null",0,0,0);
         flip1.flipCoin("Tom",2);
-        CoinFlip.getInstance().addStats(flip1);
+        CoinFlipStats flip2 = new CoinFlipStats("timedate","null",0,0,0);
+        flip2.flipCoin("Jerry",1);
+        myList.add(flip1);
+        myList.add(flip2);
 
 
 
@@ -51,9 +59,8 @@ public class flipHistory extends AppCompatActivity {
             super(flipHistory.this,R.layout.flip_items,myList );
         }
 
-        @NonNull
         @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        public View getView(int position,  View convertView, ViewGroup parent) {
             View itemView = convertView;
             if(itemView == null){
                 itemView = getLayoutInflater().inflate(R.layout.flip_items,parent,false);
@@ -65,11 +72,13 @@ public class flipHistory extends AppCompatActivity {
             ImageView imageView =(ImageView)itemView.findViewById(R.id.imageView);
             imageView.setImageResource(CurrentStats.getIconID());
 
+            /*TextView makeText = (TextView) itemView.findViewById(R.id.history_text);
+            makeText.setText(CurrentStats.getFlipTime());
 
-
-
-
-
+            TextView makeText1 = (TextView) itemView.findViewById(R.id.textView3);
+            String outputHistory = CurrentStats.getChildName()+" chose "+ interpretInt(CurrentStats.getChoice())+" and the result was "
+                    +interpretInt(CurrentStats.getResult());
+            makeText1.setText(outputHistory);*/
 
             return itemView;
 
@@ -86,6 +95,16 @@ public class flipHistory extends AppCompatActivity {
     public static Intent makeIntent(Context context) {
         return new Intent(context, flipHistory.class);
     }
+
+    public String interpretInt(int choiceOrresult){
+        if(choiceOrresult ==1){
+            return "head";
+        }
+        else{
+            return "tail";
+        }
+    }
+
 
 
 
