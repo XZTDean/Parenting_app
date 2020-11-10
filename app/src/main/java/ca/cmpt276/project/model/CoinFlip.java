b9ace00c68b8c9bef14a40016b97724a64c91688
Neil <xda22@sfu.ca>
2020-11-09 16:21:55 -0800

    Update Flip history so that it can display previous history the next time user opens the app.

--- app/src/main/java/ca/cmpt276/project/model/CoinFlip.java
+++ app/src/main/java/ca/cmpt276/project/model/CoinFlip.java
@@ -1,5 +1,9 @@
 package ca.cmpt276.project.model;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -12,7 +16,7 @@ public class CoinFlip {
 
     private static CoinFlip instance;
     private String choiceMade;
-
+    public final String Flip_KEY = "FlipList";
     public static CoinFlip getInstance() {
         if (instance == null){
             instance = new CoinFlip();
@@ -46,24 +50,17 @@ public class CoinFlip {
         FlipList.add(c);
     }
 
-    //store all the records in list to a string array which can be displayed in UI later.
-    public String[] StoreInList(){
-        String[] tempList = new String[FlipList.size()];
-        for(int i = 0; i<FlipList.size();i++){
-            tempList[i]= "at "+ FlipList.get(i).getFlipTime()+" "+ FlipList.get(i).getChildName()+
-                    " gets to pick "+interpretInt(FlipList.get(i).getChoice())+
-                    " and the result turns out to be "+interpretInt(FlipList.get(i).getResult());
-            if(FlipList.get(i).getChoice() == FlipList.get(i).getResult()){
-                tempList[i] = tempList[i]+" wining!";
-            }
-            else{
-                tempList[i]= tempList[i]+" losing!";
-            }
-
-        }
-        return tempList;
-
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(FlipList);
     }
+
+    public void loadFromJson(String json) {
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<ArrayList<Child>>(){}.getType();
+        FlipList = gson.fromJson(json, collectionType);
+    }
+
 
 
     /*interpret the integers in choice and result. If the integer is 1, it presents Head.
