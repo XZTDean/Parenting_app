bd61f5b3c17648407ad425cf353e0d1061b88c87
zetix <zetix@sfu.ca>
2020-11-17 16:44:17 +0800

    Create TaskListActivity

---
+++ app/src/main/java/ca/cmpt276/project/UI/TaskListActivity.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.project.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.project.R;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+    }
+}
