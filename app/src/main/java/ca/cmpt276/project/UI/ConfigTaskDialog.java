ac818deb7529168699cc7365de17755ec6ef51a7
zetix <zetix@sfu.ca>
2020-11-19 07:30:59 +0800

    Create task detail dialog

---
+++ app/src/main/java/ca/cmpt276/project/UI/ConfigTaskDialog.java
@@ -0,0 +1,31 @@
+package ca.cmpt276.project.UI;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.cmpt276.project.R;
+
+public class ConfigTaskDialog extends DialogFragment {
+    private View view;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        view = inflater.inflate(R.layout.config_task_dialog, null);
+
+        builder.setView(view);
+        return builder.create();
+    }
+}
